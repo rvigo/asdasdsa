834531925fa1ecf1f440253a99cf6412b0b95944
Rafael Vigo <rvigo07+github@gmail.com>
2026-04-09 12:30:18 -0300

    replace rustreleaser with plain GitHub release + repository_dispatch
    - strip build matrix and rustreleaser from build_and_release.yaml
    - release job now creates gh release with checksums.txt and triggers homebrew-asdasdsa via repository_dispatch
    - remove dead files: .goreleaser.yaml, rustreleaser.yaml, .github/utils/

--- .github/utils/dummy.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func main() {
-	// this is a workaround to release rust binaries with goreleaser
-	// ref: https://jondot.medium.com/shipping-rust-binaries-with-goreleaser-d5aa42a46be0s
-}
